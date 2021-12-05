bbd012a704cf289b2f1c2e2ca9d830cb374ec0ad
Jerome Nelson <jerome-nelson@users.noreply.github.com>
2021-12-05 16:10:03 +0000

    feat(*): add file parser and tests

--- aoc.go
+++ aoc.go
@@ -1,24 +1,74 @@
 package aoc
 
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"strconv"
+)
+
 // Points for Chapter 1
 // 1. Need a way to send input to script
-// 2. Data is given as a HTML page - so need to scrape that
-// 3. Need to look at the previous data when reviewing current data (Linked List type)
-// 4. Need to keep a count of all increased times
-
-func DepthTracker(listOfDepths []int) int {
+// 2. Data is given as a Txt File - so need to scrape that
+// 3. Need to keep a count of all increased times
+func DepthTracker(listOfDepths []string) int {
 	tracker := 0
-	for order, currentDepth := range listOfDepths {
-		var previousDepth int
+	for order, depth := range listOfDepths {
 		if order == 0 {
 			continue
 		}
 
-		previousDepth = listOfDepths[order-1]
+		currentDepth, err := strconv.Atoi(depth)
+		check(err)
+		previousDepth, err := strconv.Atoi(listOfDepths[order-1])
+		check(err)
+
 		if previousDepth < currentDepth {
 			tracker += 1
 		}
 
 	}
 	return tracker
+}
+
+func GetFile(path string) []string {
+
+	var (
+		data   *os.File
+		err    error
+		part   []byte
+		prefix bool
+		lines  []string
+	)
+	data, err = os.Open(path)
+	check(err)
+	defer data.Close()
+
+	reader := bufio.NewReader(data)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+
+			if err == io.EOF {
+				return lines
+			}
+
+			check(err)
+		}
+
+		buffer.Write(part)
+		if !prefix {
+			lines = append(lines, buffer.String())
+			buffer.Reset()
+		}
+
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
--- aoc_test.go
+++ aoc_test.go
@@ -3,10 +3,33 @@ package aoc
 import "testing"
 
 func TestDepthTracker(t *testing.T) {
-	mock := []int{1, 2, 1, 2}
+	mock := []string{"1", "2", "1", "2"}
 	depth := DepthTracker(mock)
 
 	if depth != 2 {
 		t.Errorf("DepthTracking should be 2, instead got: %d", depth)
 	}
+}
+
+func TestGetFileParse(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			return
+		}
+		t.Error("Something went wrong with files")
+	}()
+
+	GetFile("mocks/chapter-one.txt")
+}
+
+func TestGetFileError(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Error("Error wasn't called even though path was wrong")
+			return
+		}
+		t.Log("The call should fail - path was wrong")
+	}()
+
+	GetFile("mock/chapter-ones.txt")
 }
